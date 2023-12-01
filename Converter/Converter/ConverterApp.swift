32e76ce3faa2f7b4c98dffc82775002b644f279b
tima <sikorskitimofei@gmail.com>
2023-12-01 19:49:26 +0100

    converter challenge project created

---
+++ Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Tima Sikorski on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
