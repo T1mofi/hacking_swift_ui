0edd2cb4880d6f74b0230f8343eb02c02d228d0d
tima <sikorskitimofei@gmail.com>
2023-12-10 14:46:32 +0100

    grid stack implemented

---
+++ ViwesAndModifiers/ViwesAndModifiers/GridStack.swift
@@ -0,0 +1,26 @@
+//
+//  GridStack.swift
+//  ViwesAndModifiers
+//
+//  Created by Tima Sikorski on 10/12/2023.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
