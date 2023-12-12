1118aa711a79fba5a16199c7f943895821db133c
tima <sikorskitimofei@gmail.com>
2023-12-12 21:43:17 +0100

    rock_paper_scissors project created

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Tima Sikorski on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
