de15812e330366ef4bec2c4366cda7a54d157a6e
tima <sikorskitimofei@gmail.com>
2023-12-14 16:13:23 +0100

    move forward

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
