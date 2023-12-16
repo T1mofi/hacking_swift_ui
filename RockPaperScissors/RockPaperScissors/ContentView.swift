b323154a36871af9dae7ea33f89c155dcee92e95
tima <sikorskitimofei@gmail.com>
2023-12-16 09:08:05 +0100

    ui updated to show alerts and hide computers choice

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,34 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var choices = ["Rock", "Paper", "Scissors"]
-    var appsChoice: String {
-        choices.randomElement() ?? ""
+    struct Constants {
+        static let computerThinkingImage = "question-mark"
     }
-    var score: Int = 0
-    var round: Int = 0
+
+    private var choices = ["Rock", "Paper", "Scissors"]
+    @State private var appsChoice: String = Constants.computerThinkingImage
+    private var score: Int = 0
+    private var round: Int = 0
+
+    @State private var shouldShowWinAlert: Bool = false
+    @State private var shouldShowLoseAlert: Bool = false
 
     var body: some View {
         ZStack {
             LinearGradient(colors: [.pink, .purple], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 20) {
-                Text("Computer's chice")
+                Text("Computer's choice")
                     .font(.title)
                     .foregroundStyle(.white)
-                Image("rock")
-                Image(systemName: "question-mark")
+                Image(appsChoice.lowercased())
 
                 Spacer()
                 Spacer()
 
-                Text("You chice")
+                Text("Your choice")
                     .font(.title)
                     .foregroundStyle(.white)
                 HStack(spacing: 50) {
                     ForEach(choices, id: \.self) { choice in
                         Button {
-
+                            playRound(choice)
                         } label: {
                             Image(choice.lowercased())
                         }
@@ -44,10 +48,33 @@ struct ContentView: View {
 
                 Spacer()
 
-                Text("Score: \(score)")
-                Text("Round: \(round)")
+                VStack {
+                    Text("Score: \(score)")
+                    Text("Round: \(round)")
+                }
             }
             .padding()
+
+            .alert("Congrats!", isPresented: $shouldShowWinAlert, actions: {
+                Button("Continue") {
+                    appsChoice = Constants.computerThinkingImage
+                }
+            })
+            .alert("Oops!", isPresented: $shouldShowLoseAlert, actions: {
+                Button("Continue") {
+                    appsChoice = Constants.computerThinkingImage
+                }
+            })
+        }
+    }
+
+    func playRound(_ choice: String) {
+        guard let computersChoice = choices.randomElement() else { return }
+        appsChoice = computersChoice
+        if (appsChoice.lowercased() == choice.lowercased()) {
+            shouldShowWinAlert = true
+        } else {
+            shouldShowLoseAlert = true
         }
     }
 }
