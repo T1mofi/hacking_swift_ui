03f252e541102bf8b84ac89cd6684eae0239d487
tima <sikorskitimofei@gmail.com>
2023-12-20 07:38:57 +0100

    CoreML model used to make predictions

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Tima Sikorski on 17/12/2023.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,7 +13,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
 
-    @State private var showPerfectBedTimeAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var isPerfectBedTimeAlertShown = false
 
     var body: some View {
         NavigationStack {
@@ -38,14 +41,33 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
-            .alert("Your perfect bed time", isPresented: $showPerfectBedTimeAlert) {
+            .alert(alertTitle, isPresented: $isPerfectBedTimeAlertShown) {
                 Button("OK") {}
+            } message: {
+                Text(alertMessage)
             }
         }
     }
 
     func calculateBedtime() {
-        showPerfectBedTimeAlert = true
+        do {
+            let config = MLModelConfiguration()
+            let model = try BetterRestRegressor(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        isPerfectBedTimeAlertShown = true
     }
 }
 
