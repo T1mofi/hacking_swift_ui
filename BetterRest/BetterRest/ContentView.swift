2142142474088e3675eda1351b9bdd07adafc647
tima <sikorskitimofei@gmail.com>
2023-12-22 10:14:18 +0100

    ui polished

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -28,24 +28,27 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
 
-                Text("Desired amount of sleep")
-                    .font(.headline)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
 
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-
-                Text("Daily coffee intake")
-                    .font(.headline)
-
-                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect:true)", value: $coffeeAmount, in: 1...20)
+                }
             }
-            .padding()
             .navigationTitle("BetterRest")
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
