653d823f42361ed7ff804d3d2b27e374c4ab4ccb
tima <sikorskitimofei@gmail.com>
2023-12-27 11:58:40 +0100

    edutainment project created

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Tima Sikorski on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
