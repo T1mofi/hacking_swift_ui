21d57d2221ac7e0bbcab9f9fd80d8e72d37d0ab7
tima <sikorskitimofei@gmail.com>
2024-01-14 15:13:59 +0100

    main screen ui implemented

---
+++ Moonshot/Moonshot/Color-Theme.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeStyleExtension.swift
+//  Moonshot
+//
+//  Created by Tima Sikorski on 14/01/2024.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
