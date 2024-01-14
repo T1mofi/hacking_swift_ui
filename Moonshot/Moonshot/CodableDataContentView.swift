dfc1be1c74df326fd12587d137648342d82675ea
tima <sikorskitimofei@gmail.com>
2024-01-14 14:05:00 +0100

    codable data

---
+++ Moonshot/Moonshot/CodableDataContentView.swift
@@ -0,0 +1,45 @@
+//
+//  CodableDataContentView.swift
+//  Moonshot
+//
+//  Created by Tima Sikorski on 14/01/2024.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct CodableDataContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+#Preview {
+    CodableDataContentView()
+}
+
