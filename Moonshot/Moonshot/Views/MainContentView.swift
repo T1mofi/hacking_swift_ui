d1097a7366bd955b146e1e978f105acee68882b1
tima <sikorskitimofei@gmail.com>
2024-01-18 22:18:08 +0100

    bar button added

---
+++ Moonshot/Moonshot/Views/MainContentView.swift
@@ -0,0 +1,73 @@
+//
+//  CodableDataContentView.swift
+//  Moonshot
+//
+//  Created by Tima Sikorski on 14/01/2024.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .navigationBarItems(
+                trailing: Button(action: {
+                    // TODO: - Change style here
+                }) {
+                    Image(systemName: "gear")
+                }
+            )
+        }
+    }
+}
+
+#Preview {
+    MainContentView()
+}
+
