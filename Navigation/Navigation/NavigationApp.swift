6f9f67fae3cf53202de07d054d0ef9a8e3c46759
tima <sikorskitimofei@gmail.com>
2024-01-19 18:30:56 +0100

    navigation project created

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Tima Sikorski on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
