9c00c9d42c43ba1082d1207a45eef4b33d27fefd
tima <sikorskitimofei@gmail.com>
2024-01-20 10:41:45 +0100

    navigating different data types with navigationPath

---
+++ Navigation/Navigation/DifferentPathContentView.swift
@@ -0,0 +1,47 @@
+//
+//  DifferentPathContentView.swift
+//  Navigation
+//
+//  Created by Tima Sikorski on 20/01/2024.
+//
+
+import SwiftUI
+
+struct DifferentPathContentView: View {
+    @State private var path = NavigationPath()
+
+    var body: some View {
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
+                }
+
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
+        }
+
+    }
+}
+
+#Preview {
+    DifferentPathContentView()
+}
+
