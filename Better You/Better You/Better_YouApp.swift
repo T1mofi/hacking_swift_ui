ac7e23e3e8a5d74d67e784a84b0449c1f561377f
tima <sikorskitimofei@gmail.com>
2024-01-21 10:53:44 +0100

    Better you challange project - innitial commit

---
+++ Better You/Better You/Better_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_YouApp.swift
+//  Better You
+//
+//  Created by Tima Sikorski on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Better_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
