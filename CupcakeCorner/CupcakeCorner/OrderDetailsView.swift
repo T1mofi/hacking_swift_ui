ba985aadb95fbc6c6f1f064b8973f5cf7cd7adf2
tima <sikorskitimofei@gmail.com>
2024-02-07 21:44:54 +0100

    order details view updated

--- CupcakeCorner/CupcakeCorner/OrderDetailsView.swift
+++ CupcakeCorner/CupcakeCorner/OrderDetailsView.swift
@@ -12,28 +12,35 @@ struct OrderDetailsView: View {
 
     var body: some View {
         NavigationStack {
-            Section {
-                Picker("Select your cake type", selection: $order.type) {
-                    ForEach(Order.types.indices, id: \.self) { type in
-                        Text(Order.types[type])
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
                     }
                 }
 
-                Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
-            }
-            Section {
-                Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
-
-                if order.specialRequestEnabled {
-                    Toggle("Add extra frosting", isOn: $order.extraFrosting)
-                    Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
                 }
             }
-            NavigationLink("Delivery details") {
-                AddressView(order: order)
-            }
+            .navigationTitle("Cupcake Corner")
         }
-        .navigationTitle("Cupcake Corner")
     }
 }
 
