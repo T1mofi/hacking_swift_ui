4c646962e9b02589ae3d0f5c1097086c5cb83c4c
tima <sikorskitimofei@gmail.com>
2024-02-09 19:57:33 +0100

    post method implemented

--- CupcakeCorner/CupcakeCorner/CheckouView.swift
+++ CupcakeCorner/CupcakeCorner/CheckouView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
 
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+
     var body: some View {
         ScrollView {
             VStack {
@@ -25,13 +28,43 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
 
-                Button("Place Order", action: { })
+                Button("Place Order", action: {
+                    Task {
+                        await placeOrder()
+                    }
+                })
                     .padding()
+            }
+            .alert("Thank you!", isPresented: $showingConfirmation) {
+                Button("OK") {}
+            } message: {
+                Text(confirmationMessage)
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+    }
+
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let receivedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(receivedOrder.quantity)x \(Order.types[receivedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
     }
 }
 
