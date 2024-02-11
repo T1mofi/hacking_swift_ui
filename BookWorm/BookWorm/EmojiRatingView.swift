844c784b6a9debd9a960b858eb7fc52dd4b90088
tima <sikorskitimofei@gmail.com>
2024-02-11 18:04:22 +0100

    Navigation link added to content view

---
+++ BookWorm/BookWorm/EmojiRatingView.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiRatingView.swift
+//  BookWorm
+//
+//  Created by Tima Sikorski on 11/02/2024.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("1")
+        case 2:
+            Text("2")
+        case 3:
+            Text("3")
+        case 4:
+            Text("4")
+        default:
+            Text("5")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
