d8bd610ec463bb850a1ba18c0a389696e0bcf554
tima <sikorskitimofei@gmail.com>
2024-02-15 18:53:37 +0100

    try out predicate

--- SwiftyStorage/SwiftyStorage/ContentView.swift
+++ SwiftyStorage/SwiftyStorage/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \User.name) var users: [User]
+    @Query(filter: #Predicate { user in
+        user.name.localizedStandardContains("R") &&
+        user.city == "London"
+    }, sort: \User.name) var users: [User]
     @State private var path = [User]()
 
     var body: some View {
@@ -29,6 +32,18 @@ struct ContentView: View {
                     let user = User(name: "", city: "", joinDate: .now)
                     modelContext.insert(user)
                     path = [user]
+                }
+                Button("Add Samples") {
+                    try? modelContext.delete(model: User.self)
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
