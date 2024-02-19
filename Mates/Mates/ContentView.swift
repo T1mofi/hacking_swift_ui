6aae6b7193812a007c816f50049a14e3b6145fe3
tima <sikorskitimofei@gmail.com>
2024-02-19 18:42:48 +0100

    mates project initialized

---
+++ Mates/Mates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mates
+//
+//  Created by Tima Sikorski on 19/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mates/Mates/MatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatesApp.swift
+//  Mates
+//
+//  Created by Tima Sikorski on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
