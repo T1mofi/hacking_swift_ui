6fec4e177f4bb95ebe4b10973885449d8e87cd80
tima <sikorskitimofei@gmail.com>
2024-02-22 16:42:27 +0100

    Instafilter project created

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Tima Sikorski on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
