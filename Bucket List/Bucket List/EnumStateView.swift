d76e3dc536e80fb3450ff32b5bfb3087d6d56eeb
tima <sikorskitimofei@gmail.com>
2024-03-04 20:06:04 +0100

    different views for different swift cases

---
+++ Bucket List/Bucket List/EnumStateView.swift
@@ -0,0 +1,49 @@
+//
+//  EnumStateView.swift
+//  Bucket List
+//
+//  Created by Tima Sikorski on 04/03/2024.
+//
+
+import SwiftUI
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct EnumStateView: View {
+    @State private var loadingState = LoadingState.failed
+
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+    }
+}
+
+#Preview {
+    EnumStateView()
+}
