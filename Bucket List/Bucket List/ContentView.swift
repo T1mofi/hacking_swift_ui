203574f72cdcc18fb591816359d20abc60d0e75c
tima <sikorskitimofei@gmail.com>
2024-03-04 19:58:56 +0100

    read and write to documents directory

--- Bucket List/Bucket List/ContentView.swift
+++ Bucket List/Bucket List/ContentView.swift
@@ -25,8 +25,17 @@ struct ContentView: View {
     ].sorted()
 
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstName)")
+        Button("Read and Write") {
+            let data = Data("Test Message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
