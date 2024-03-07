bc75e17871ddfcb4285a62ff96ed9c9ccbd317e7
tima <sikorskitimofei@gmail.com>
2024-03-07 07:46:33 +0100

    data requested from the internet

--- Bucket List/Bucket List/EditView.swift
+++ Bucket List/Bucket List/EditView.swift
@@ -68,6 +68,26 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            fatalError("Failed to create URL")
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let result = try JSONDecoder().decode(Result.self, from: data)
+            pages = result.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
         }
     }
 }
