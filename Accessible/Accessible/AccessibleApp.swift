b1ce34366da03db9eaeeaf359a78f8e6e15d62e1
tima <sikorskitimofei@gmail.com>
2024-03-13 08:55:26 +0100

    Accessable project started

---
+++ Accessible/Accessible/AccessibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibleApp.swift
+//  Accessible
+//
+//  Created by Tima Sikorski on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessible/Accessible/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Accessible
+//
+//  Created by Tima Sikorski on 13/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+
+    @State private var selectedPicture = Int.random(in: 0...3)
+
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
