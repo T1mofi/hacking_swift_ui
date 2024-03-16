e4aaec1ab4f178ca4b3b443c9b003d117e63cdac
tima <sikorskitimofei@gmail.com>
2024-03-16 11:23:59 +0100

    control with voicer

--- Accessible/Accessible/ContentView.swift
+++ Accessible/Accessible/ContentView.swift
@@ -11,30 +11,10 @@ struct ContentView: View {
     @State private var value = 10
 
     var body: some View {
-        VStack {
-            Text("Value: \(value)")
-
-            Button("Increment") {
-                value += 1
-            }
-
-            Button("Decrement") {
-                value -= 1
-            }
+        Button("John Fitzgerald Kennedy") {
+            print("Button tapped")
         }
-        .accessibilityElement()
-        .accessibilityLabel("Value")
-        .accessibilityValue(String(value))
-        .accessibilityAdjustableAction { direction in
-            switch direction {
-            case .increment:
-                value += 1
-            case .decrement:
-                value -= 1
-            default:
-                print("Not handled.")
-            }
-        }
+        .accessibilityInputLabels(["John Fitzgerald Kennedy", "Kennedy", "JFK"])
     }
 }
 
