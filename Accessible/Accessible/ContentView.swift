984b336532833dc6c2dc16190b31801240ace62f
tima <sikorskitimofei@gmail.com>
2024-03-16 11:14:53 +0100

    increment/decrement - VoiceOver

--- Accessible/Accessible/ContentView.swift
+++ Accessible/Accessible/ContentView.swift
@@ -8,32 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var value = 10
 
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
+        VStack {
+            Text("Value: \(value)")
+
+            Button("Increment") {
+                value += 1
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled.")
+            }
+        }
     }
 }
 
