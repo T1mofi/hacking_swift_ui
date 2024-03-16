4782ee9d0572e96d3ab708fd996492ee5102953c
tima <sikorskitimofei@gmail.com>
2024-03-16 11:44:06 +0100

    bookworm accessible

--- BookWorm/BookWorm/RatingView.swift
+++ BookWorm/BookWorm/RatingView.swift
@@ -33,7 +33,20 @@ struct RatingView: View {
                     image(for: number)
                         .foregroundStyle(number > rating ? offColor : onColor)
                 }
-                .buttonStyle(.plain)
+            }
+        }
+        .buttonStyle(.plain)
+        .accessibilityElement()
+        .accessibilityLabel(label)
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
             }
         }
     }
