e9990499b34b4bf0699c7fa2be4bab95c8ecaddf
tima <sikorskitimofei@gmail.com>
2024-03-20 18:53:09 +0100

    Notification

---
+++ Hot Prospects/Hot Prospects/LocalNotificationView.swift
@@ -0,0 +1,45 @@
+//
+//  LocalNotificationView.swift
+//  Hot Prospects
+//
+//  Created by Tima Sikorski on 20/03/2024.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct LocalNotificationView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                // show this notification five seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
+
+#Preview {
+    LocalNotificationView()
+}
