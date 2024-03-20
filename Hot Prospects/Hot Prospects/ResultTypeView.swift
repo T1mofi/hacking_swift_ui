a63d22f09b7b4a0feb575058b71748b30da3a245
tima <sikorskitimofei@gmail.com>
2024-03-20 18:15:26 +0100

    result struct

---
+++ Hot Prospects/Hot Prospects/ResultTypeView.swift
@@ -0,0 +1,39 @@
+//
+//  ResultTypeView.swift
+//  Hot Prospects
+//
+//  Created by Tima Sikorski on 20/03/2024.
+//
+
+import SwiftUI
+
+struct ResultTypeView: View {
+    @State private var output = ""
+
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        let result = await fetchTask.result
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
+    }
+}
+
+#Preview {
+    ResultTypeView()
+}
