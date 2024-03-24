2c881c21ed80efc6572a53a15bff4c5866c5c59b
tima <sikorskitimofei@gmail.com>
2024-03-24 12:47:47 +0100

    qr creation

--- Hot Prospects/Hot Prospects/MyInfoView.swift
+++ Hot Prospects/Hot Prospects/MyInfoView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MyInfoView: View {
+    @AppStorage("name") private var name = "Anonymus"
+    @AppStorage("email") private var email = "you@yourdomain.com"
+
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+
+                TextField("Email address", text: $email)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
