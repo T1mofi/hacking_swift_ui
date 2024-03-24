63293f5502f72c752769cf2d549332ed53bf84bb
tima <sikorskitimofei@gmail.com>
2024-03-24 13:04:14 +0100

    qr scanning

--- Hot Prospects/Hot Prospects/ProspectsView.swift
+++ Hot Prospects/Hot Prospects/ProspectsView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import CodeScanner
 
 enum FilterType {
     case none, contacted, uncontacted
@@ -17,6 +18,8 @@ struct ProspectsView: View {
 
     @Query(sort: \Prospect.name) var prospects: [Prospect]
     @Environment(\.modelContext) var modelContext
+
+    @State private var isShowingScanner = false
 
     var title: String {
         switch filter {
@@ -55,10 +58,27 @@ struct ProspectsView: View {
                 .navigationTitle(title)
                 .toolbar {
                     Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                        modelContext.insert(prospect)
+                        isShowingScanner = true
                     }
                 }
+        }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+        }
+    }
+
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
