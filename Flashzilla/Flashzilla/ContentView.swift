60a7bca77ca49d92b27d253ff68a5d74f6c36b6d
tima <sikorskitimofei@gmail.com>
2024-03-25 21:46:20 +0100

    Stack of cards

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .onTapGesture(count: 2) {
-                    print("DoupleTap recognized")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
                 }
+            }
         }
-        .padding()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
     }
 }
 
