021cdde7ae9ca5d28b8cb158cf5d941cee10eeba
tima <sikorskitimofei@gmail.com>
2024-03-25 21:25:07 +0100

    Card view added

---
+++ Flashzilla/Flashzilla/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Tima Sikorski on 25/03/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+
+    @State private var isShowingAnswer = false
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .shadow(radius: 10)
+
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card: .example)
+}
