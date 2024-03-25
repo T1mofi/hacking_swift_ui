e4bb08deeb21709ec5e17005959594e8e0dd56aa
tima <sikorskitimofei@gmail.com>
2024-03-25 15:57:03 +0100

    Card model implemented

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Tima Sikorski on 25/03/2024.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
