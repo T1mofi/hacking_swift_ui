071c9184393f96c0412424c17e5bf6dcac5eff26
tima <sikorskitimofei@gmail.com>
2024-03-26 22:09:16 +0100

    start layout project

---
+++ LaidOut/LaidOut/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LaidOut
+//
+//  Created by Tima Sikorski on 26/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color.red
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaidOut/LaidOut/LaidOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaidOutApp.swift
+//  LaidOut
+//
+//  Created by Tima Sikorski on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LaidOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
