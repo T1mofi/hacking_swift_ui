088889dc249e862f1d3e14781fb0eac12b1c0188
tima <sikorskitimofei@gmail.com>
2024-03-28 18:36:28 +0100

    custom coordinate space

---
+++ LaidOut/LaidOut/CoordinateSystemView.swift
@@ -0,0 +1,50 @@
+//
+//  CoordinateSystemView.swift
+//  LaidOut
+//
+//  Created by Tima Sikorski on 28/03/2024.
+//
+
+import SwiftUI
+
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct CoordinateSystemView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
+    }
+}
+
+#Preview {
+    CoordinateSystemView()
+}
--- LaidOut/LaidOut/LaidOutApp.swift
+++ LaidOut/LaidOut/LaidOutApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LaidOutApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CoordinateSystemView()
         }
     }
 }
