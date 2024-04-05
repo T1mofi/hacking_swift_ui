1033a2bfdf1bad0cc461bc833d604f5be8e608d5
tima <sikorskitimofei@gmail.com>
2024-04-05 19:38:39 +0200

    snow seeker project started

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Tima Sikorski on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
