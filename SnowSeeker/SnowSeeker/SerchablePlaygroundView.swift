4baefc9253946367b9ef576a44487f2d46ced48a
tima <sikorskitimofei@gmail.com>
2024-04-07 13:35:32 +0200

    searchable

---
+++ SnowSeeker/SnowSeeker/SerchablePlaygroundView.swift
@@ -0,0 +1,35 @@
+//
+//  SerchablePlaygroundView.swift
+//  SnowSeeker
+//
+//  Created by Tima Sikorski on 07/04/2024.
+//
+
+import SwiftUI
+
+struct SerchablePlaygroundView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { $0.localizedStandardContains(searchText) }
+        }
+    }
+
+    var body: some View {
+        NavigationStack {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+}
+
+#Preview {
+    SerchablePlaygroundView()
+}
