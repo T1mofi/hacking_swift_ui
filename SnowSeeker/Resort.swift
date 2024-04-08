65898fa195d5d7fb50b7f0021f25213786db8608
tima <sikorskitimofei@gmail.com>
2024-04-08 20:32:17 +0200

    Add resort model

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,22 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Tima Sikorski on 08/04/2024.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+}
